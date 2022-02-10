1b58994271d95e038277e360c3e15e3eab9705f4
Itamar lebel <itamarlebel1@gmail.com>
2022-02-10 11:06:46 +0200

    Card Class UT - Create setUp

--- Card Game/test_Card_Class.py
+++ Card Game/test_Card_Class.py
@@ -1,6 +1,11 @@
 from unittest import TestCase
+from Card_Class import Card
 
 
 class TestCard(TestCase):
-    def test_get_name(self):
-        self.fail()
+    # Instantiating objects of the tested class - Card:
+    def setUp(self):
+        self.ace_of_diamond = Card(1, 1)
+        self.ace_of_club = Card(1, 4)
+        self.king_of_club = Card(13, 4)
+        self.ten_of_heart = Card(10, 3)
