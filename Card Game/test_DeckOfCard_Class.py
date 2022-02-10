e043f3266c8507fb0785876e4e7935b8d7a6265c
Itamar lebel <itamarlebel1@gmail.com>
2022-02-10 11:35:14 +0200

    DeckOfCard Class UT - Adding tests for the __init__ and card shuffle methods

--- Card Game/test_DeckOfCard_Class.py
+++ Card Game/test_DeckOfCard_Class.py
@@ -1,9 +1,41 @@
 from unittest import TestCase
 from DeckOfCard_Class import DeckOfCards
-from Card import Card
+from Card_Class import Card
 
 
 class TestDeckOfCards(TestCase):
     # Set a global object of the class for the tests
     def setUp(self):
         self.deck_of_cards = DeckOfCards()
+
+    # Test the __init__ method, that the list contains 52 items, and that the items are not the same card
+    def test__init__valid(self):
+        self.assertTrue(len(self.deck_of_cards.deck) == 52)
+        for suit in range(1, 5):
+            for value in range(1, 14):
+                card = Card(value, suit)
+                exist = False
+                for item in self.deck_of_cards.deck:
+                    if item.get_name() == card.get_name() and exist is False:
+                        exist = True
+                self.assertTrue(exist)
+
+    # Test the valid option of the card shuffle method
+    def test_card_shuffle_valid(self):
+        shuffled_deck = DeckOfCards()
+        shuffled_deck.card_shuffle()
+        self.assertNotEqual(self.deck_of_cards.deck, shuffled_deck.deck)
+
+        # Test the minimum value of cards amount in deck to shuffle
+        self.deck_of_cards.deck = []
+        self.deck_of_cards.deck.append(Card(1, 1))
+        self.deck_of_cards.deck.append(Card(1, 2))
+        DeckOfCards.card_shuffle(self.deck_of_cards)
+        self.assertTrue(self.deck_of_cards)
+
+    # Test an invalid shuffle for deck (less than 2 cards)
+    def test_card_shuffle_invalid(self):
+        self.deck_of_cards.deck = []
+        self.deck_of_cards.deck.append(Card(1, 1))
+        with self.assertRaises(AttributeError):
+            DeckOfCards.card_shuffle(self.deck_of_cards)
