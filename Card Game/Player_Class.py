35fd125b479bf9e4dacb6d5036ec62b6c9e513c1
Itamar lebel <itamarlebel1@gmail.com>
2022-02-10 10:49:28 +0200

    Player Class - Adding a method that give the player cards from the deck

--- Card Game/Player_Class.py
+++ Card Game/Player_Class.py
@@ -19,3 +19,18 @@ class Player:
     # return the name of the player
     def __str__(self):
         return f"{self.name}"
+
+    # Used to hand out cards to the player out of the deck of cards:
+    def set_hand(self, deck_of_cards: DeckOfCards):
+        """
+        This method gets DeckOfCards object as a parameter, and deals the player the amount of cards it has to get
+        Each card is unique
+        """
+        if type(deck_of_cards) != DeckOfCards:
+            raise TypeError("Deck of cards must be a DeckOfCards type!")
+        if len(deck_of_cards.deck) < self.num_of_cards:
+            raise ValueError("There's not enough cards in the deck to hand out to the player")
+        for i in range(self.num_of_cards):
+            card = deck_of_cards.deal_one()
+            if card not in self.cards:
+                self.cards.append(card)
